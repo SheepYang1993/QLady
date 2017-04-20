e56ed209508eae09a5d9a82e5f50f58741a0fdec
SheepYang <332594623@qq.com>
2017-04-20 17:44:42 +0800

    ~

--- app/src/main/java/me/sheepyang/qlady/adapter/NewAdapter.java
+++ app/src/main/java/me/sheepyang/qlady/adapter/NewAdapter.java
@@ -1,6 +1,7 @@
 package me.sheepyang.qlady.adapter;
 
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -17,20 +18,34 @@ import me.sheepyang.qlady.util.glide.GlideCircleTransform;
  */
 
 public class NewAdapter extends BaseQuickAdapter<NewEntity, BaseViewHolder> {
+    private int mScreenWidth;
+
     public NewAdapter(List<NewEntity> data) {
         super(R.layout.item_new, data);
     }
 
     @Override
     protected void convert(BaseViewHolder helper, NewEntity item) {
+        mScreenWidth = mContext.getResources().getDisplayMetrics().widthPixels;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(mScreenWidth, (int) (mScreenWidth/* * 1.25*/));
+        helper.getView(R.id.iv_desc).setLayoutParams(params);
+
         Glide.with(mContext)
                 .load("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492624497119&di=298dc98d6977a37dab24f902d091ddd2&imgtype=0&src=http%3A%2F%2Fk2.jsqq.net%2Fuploads%2Fallimg%2F1702%2F7_170228144936_2.jpg")
                 .transform(new GlideCircleTransform(mContext))
                 .placeholder(R.drawable.anim_loading_view)
                 .into((ImageView) helper.getView(R.id.iv_avatar));
         Glide.with(mContext)
-                .load("http://ww2.sinaimg.cn/mw690/006tTKPugw1f8syfu1ldgj30j60cswht.jpg")
+//                .load("http://ww2.sinaimg.cn/mw690/006tTKPugw1f8syfu1ldgj30j60cswht.jpg")
+//                .load("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492676974165&di=016cfc3b6df3f97bfc097d667e159cd7&imgtype=0&src=http%3A%2F%2Fimg05.tooopen.com%2Fimages%2F20150330%2Ftooopen_sy_83857437627.jpg")
+//                .load("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492677296016&di=1bd060483a11fa70c5ebdb3a9b648cfb&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F13%2F48%2F15%2F74w58PICvPT_1024.jpg")
+                .load("http://img1.mm131.com/pic/2889/m.jpg")
                 .centerCrop()
                 .into((ImageView) helper.getView(R.id.iv_desc));
+    }
+
+    public void updata(List<NewEntity> data) {
+        mData = data;
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/me/sheepyang/qlady/fragment/NewFragment.java
+++ app/src/main/java/me/sheepyang/qlady/fragment/NewFragment.java
@@ -3,8 +3,15 @@ package me.sheepyang.qlady.fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.lcodecore.tkrefreshlayout.RefreshListenerAdapter;
+import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
+import com.lcodecore.tkrefreshlayout.footer.LoadingView;
+import com.lcodecore.tkrefreshlayout.header.SinaRefreshView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +28,13 @@ public class NewFragment extends BaseFragment {
     private static final String ARG_PARAM1 = "param1";
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
+    @BindView(R.id.refresh_layout)
+    TwinklingRefreshLayout mRefreshLayout;
     private String mParam1;
     private NewAdapter mAdapter;
     private List<NewEntity> mData = new ArrayList<>();
+    private SinaRefreshView mHeadView;
+    private View mBannar;
 
     public NewFragment() {
 
@@ -53,19 +64,64 @@ public class NewFragment extends BaseFragment {
     @Override
     protected void init() {
         initView();
+        initListener();
         initData();
     }
 
+    private void initListener() {
+        mRefreshLayout.setOnRefreshListener(new RefreshListenerAdapter() {
+            @Override
+            public void onRefresh(TwinklingRefreshLayout refreshLayout) {
+                super.onRefresh(refreshLayout);
+                mRefreshLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mData.clear();
+                        for (int i = 0; i < 1; i++) {
+                            mData.add(new NewEntity());
+                        }
+                        mAdapter.updata(mData);
+                        mRefreshLayout.finishRefreshing();
+                    }
+                }, 3000);
+            }
+
+            @Override
+            public void onLoadMore(TwinklingRefreshLayout refreshLayout) {
+                super.onLoadMore(refreshLayout);
+                mRefreshLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 2; i++) {
+                            mData.add(new NewEntity());
+                        }
+                        mAdapter.updata(mData);
+                        mRefreshLayout.finishLoadmore();
+                    }
+                }, 3000);
+            }
+        });
+    }
+
     private void initData() {
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 1; i++) {
             mData.add(new NewEntity());
         }
+        mAdapter.updata(mData);
     }
 
     private void initView() {
+        mHeadView = new SinaRefreshView(mContext);
+        mHeadView.setArrowResource(R.drawable.ico_pink_arrow);
+        mRefreshLayout.setHeaderView(mHeadView);
+        mRefreshLayout.setBottomView(new LoadingView(mContext));
+
         mAdapter = new NewAdapter(mData);
-        mAdapter.isFirstOnly(false);
+        mAdapter.isFirstOnly(true);
         mAdapter.openLoadAnimation(BaseQuickAdapter.ALPHAIN);
+
+        mBannar = LayoutInflater.from(mContext).inflate(R.layout.layout_bannar, (ViewGroup) recyclerView.getParent(), false);
+        mAdapter.addHeaderView(mBannar);
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
