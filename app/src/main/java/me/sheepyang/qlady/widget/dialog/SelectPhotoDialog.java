4e2768c2100090d7fce1892cb60884bbf00a0170
SheepYang <332594623@qq.com>
2017-04-26 18:25:24 +0800

    ~

---
+++ app/src/main/java/me/sheepyang/qlady/widget/dialog/SelectPhotoDialog.java
@@ -0,0 +1,25 @@
+package me.sheepyang.qlady.widget.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StyleRes;
+
+/**
+ * Created by SheepYang on 2017/4/26.
+ */
+
+public class SelectPhotoDialog extends Dialog {
+    public SelectPhotoDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    public SelectPhotoDialog(@NonNull Context context, @StyleRes int themeResId) {
+        super(context, themeResId);
+    }
+
+    protected SelectPhotoDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+}
