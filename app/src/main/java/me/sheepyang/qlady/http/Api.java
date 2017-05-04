b026c4b004ce8f8c5e43daf0acab153b9f1e0898
SheepYang <332594623@qq.com>
2017-05-04 17:43:23 +0800

    ~

--- app/src/main/java/me/sheepyang/qlady/http/Api.java
+++ app/src/main/java/me/sheepyang/qlady/http/Api.java
@@ -5,11 +5,14 @@ package me.sheepyang.qlady.http;
  */
 
 public class Api {
-    public static final String HOST = "http://172.21.20.2:8080/GodGoddess";
+    public static final String HOST = "http://139.196.164.217:9083/GodGoddess";
+    //    public static final String HOST = "http://172.21.20.2:8080/GodGoddess";
     //登陆
     public static final String LOGIN = HOST + "/_check";
     //重设密码
     public static final String RESET_PASSWORD = HOST + "/tBaseUser/getPassword.action";
     //获取验证码
     public static final String GET_VERIFY_CODE = HOST + "/tBaseUser/getAcode.action";
+    //获取验证码
+    public static final String MODEL_LIST = HOST + "/tModelImglb/selectAll.action";
 }
